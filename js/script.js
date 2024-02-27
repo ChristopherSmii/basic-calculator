fdc7dc9cb909fc3410ea3df70bcbf55b0e7d2c2e
Βασίλης Χρήστος Σμυρλόγλου <94610215+ChristopherSmii@users.noreply.github.com>
2024-02-27 22:15:33 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,50 +4,100 @@ let resultt=$('#resultMain');
 //
 //
 //
+const lookAt=(key)=>{
+    for(let i=0;i<key.length;i++){
+        if(key[i]=='/'||key[i]=='*'||key[i]=='-'||key[i]=='+'){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+// calculation function !!
+
 
 const calFunction=(stringMain)=>{
-let a,b,character,help;
-
-a='';
-b='';
-help=false;
-if(limitValue<=18){
-for(let i=0;i<stringMain.length;i++){
+    let a,b,character,help,help1,help2,r;
+    help1='';
+    help2='';
+    a='';
+    b='';
+    help=0;
     
-    if(stringMain[i]>=0&&stringMain[i]<=9&&help==false){
-        a=a+stringMain[i];
+    let arrayOperations=[];
+    for(let i=0;i<stringMain.length;i++){
+        if(stringMain[i]=='*'||stringMain[i]=='/'||stringMain[i]=='+'||stringMain[i]=='-'){
+            arrayOperations[i]=stringMain[i];
+        }
+        else{
+            arrayOperations[i]='';
+        }
     }
-    else if(help==false&&(stringMain[i]=='+'||stringMain[i]=='-'||stringMain[i]=='*'||stringMain[i]=='/')){
-        character=stringMain[i];
-        help=true;
+    //
+    //
+    //
+    for(let i=0;i<stringMain.length;i++){
+        if(arrayOperations[i]=='*'||arrayOperations[i]=='/'){
+            character=arrayOperations[i];
+        }
+        else if(character!='/'&&character!='*'&&(arrayOperations[i]=='+'||arrayOperations[i]=='-')){
+            character=arrayOperations[i];
+        }
+    }
+    //
+    //
+    //
+    for(let i=0;i<stringMain.length;i++){
         
-    }
-    else if(stringMain[i]>=0&&stringMain[i]<=9&&help==true){
-        b=b+stringMain[i];
-    }
+        if(help==0&&(stringMain[i]=='*'||stringMain[i]=='/'||stringMain[i]=='+'||stringMain[i]=='-')&&stringMain[i]!=character){
+            help1=help1+a+stringMain[i];
+            a='';
+        }
+        else if(help==0&&(stringMain[i]=='*'||stringMain[i]=='/'||stringMain[i]=='+'||stringMain[i]=='-')&&stringMain[i]==character){
+            help=1;
+        }
+        else if(help==1&&(stringMain[i]=='*'||stringMain[i]=='/'||stringMain[i]=='+'||stringMain[i]=='-')){
+            help=2;
+        }
 
+        //
+        //
+        //
+        if(help==0&&!(stringMain[i]=='*'||stringMain[i]=='/'||stringMain[i]=='+'||stringMain[i]=='-')){
+            a=a+stringMain[i];
+        }
+        else if(help==1&&!(stringMain[i]=='*'||stringMain[i]=='/'||stringMain[i]=='+'||stringMain[i]=='-')){
+            b=b+stringMain[i];
+        }
+        else if(help==2){
+            help2=help2+stringMain[i];
+        }
+    }
+    //
+    //
+    //
+    console.log(arrayOperations);
+    console.log(character);
+    if(character=='+'){
+        r=a*1+b*1;
+    }
+    else if(character=='-'){
+        r=a*1-b*1;
+    }
+    else if(character=='*'){
+        r=(a*1)*(b*1);
+    }
+    else if(character=='/'){
+        r=(a*1)/(b*1);
+    }
+    return(help1+r+help2);
 }
-limitValue++;
-}
-a=a*1;
-b=b*1;
 
-if(character=='+'){
-    resultt.text(a+b);
-}
-else if(character=='-'){
-    resultt.text(a-b);
-}
-else if(character=='*'){
-    resultt.text(a*b);
-}
-else if(character=='/'){
-    resultt.text(a/b);
-}
-else{
-    alert('Not valid input');
-}
-}
+
+// calculation function !!
+
+
 
 // $('#calculatorText').on('keypress', function (event) {
 //     if(event.key==='Enter'){
@@ -66,23 +116,30 @@ $('button').on('click', function () {
         limitValue=0;
     }
     else if($(this).text()=='='){
-        //calFunction(calText);
+        //Main Idea?
+        //
+        //
+        //  <<<------>>>
+        while(lookAt(calText)){
+            calText=calFunction(calText);
+        }
+            resultt.text(calText);
         
+
+
         limitValue=0;
-        let calApi="https://newton.now.sh/api/v2/simplify/"+calText;
-        apiFunction(calApi);
+        // let calApi="https://newton.now.sh/api/v2/simplify/"+calText;
+        // apiFunction(calApi);
         calText='';
     }
     $('#calculatorText').text(calText);
     });
 
-    function apiFunction(aKey){
-        console.log(aKey);
-        fetch(aKey)
-        .then(res=>res.json())
-        .then((data)=>{
-            resultt.text(data.result);
+    // function apiFunction(aKey){
+    //     console.log(aKey);
+    //     fetch(aKey).then(res=>res.json()).then((data)=>{
+    //         resultt.text(data.result);
             
-        })
+    //     })
         
-    }
+    // }
